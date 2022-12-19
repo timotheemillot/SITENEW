dae7349e139f0b043779be00c2ab25a984845cfb
timotheemillot <timothee.millot@gmail.com>
2022-12-19 21:08:14 +0100

    heuu cha bug

---
+++ views/View.php
@@ -0,0 +1,54 @@
+
+<?php
+
+class View
+{
+    private string $fichier;
+    private string $titre;
+
+    public function __construct(string $action) {
+        // Détermination du nom du fichier vue à partir de l'action
+        $this->fichier = "views/vue" . $action . ".php";
+        $this->titre = $action;
+    }
+
+    public function generer(array $donnees) {
+        // Génération de la partie spécifique de la vue
+        $contenu = $this->genererFichier($this->fichier, $donnees);
+        // Génération du gabarit commun utilisant la partie spécifique
+        $vue = $this->genererFichier('views/gabarit.php',
+          array('titre' => $this->titre, 'contenu' => $contenu));
+        // Renvoi de la vue au navigateur
+        echo $vue;
+      }
+
+    public function generer2(array $donnees)
+    {
+      // Génération de la partie spécifique de la vue
+      $contenu = $this->genererFichier($this->fichier, $donnees);
+      // Génération du gabarit commun utilisant la partie spécifique
+      $vue = $this->genererFichier('views/gabarit2.php',
+      array('titre' => $this->titre, 'contenu' => $contenu));
+      // Renvoi de la vue au navigateur
+      echo $vue;
+    }
+
+      private function genererFichier(string $fichier, array $donnees) {
+        if (file_exists($fichier)) {
+          // Rend les éléments du tableau $donnees accessibles dans la vue
+          // Voir la documentation de extract
+          extract($donnees);
+          // Démarrage de la temporisation de sortie
+          ob_start();
+          // Inclut le fichier vue
+          // Son résultat est placé dans le tampon de sortie
+          require $fichier;
+          // Arrêt de la temporisation et renvoi du tampon de sortie
+          return ob_get_clean();
+        }
+        else {
+          throw new Exception("Fichier '$fichier' introuvable");
+        }
+      }
+}
+?>
---
+++ views/gabarit.php
@@ -0,0 +1,51 @@
+
+<!doctype html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8"/>
+<link rel="stylesheet" href="public/css/main.css"/>
+<link rel="stylesheet" href="public/css/objects.css"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" sizes="16x16" href="public/img/logo.png">
+<title><?= $titre ?></title>
+</head>
+<body>
+<header>
+<!-- Menu -->
+<nav>
+
+    <a href="index.php"><img src="public/img/logo.png" class="logo" alt=""></a>
+    <ul>
+        <li><a href="index.php?action=vehicule">Véhicule</a></li>
+        <li><a href="#">Réservation</a></li>
+        <li><a href="#">Statistique</a></li>
+    </ul>
+    <a href="index.php?action=compte"><img  src="public/img/compte.png" class="compte" alt="" ></a>
+</nav>
+
+</header>
+<!-- #contenu -->
+<main id="contenu">
+    <?= $contenu ?>
+</main>
+<footer>
+        <div id="divtopfooter">
+        <div id="divhelp">
+            <span id="needhelptext">Besoin d'aide ? </span>
+            <a id="needhelplink" href="#">Contactez-nous !</a> 
+        </div>
+        <a id="policonf" href="../PoliConf/policonf.html">Politique de confidentialité<a>
+        <div id="langue">
+            <label for="langues">Langue:</label>
+            <select name="langues" id="">
+            <option value="">Français</option>
+            <option value="">Anglais</option>
+            </select>
+        </div>
+        </div>
+        
+        <p id="copyright">Copyright 2022 -- AirPur -- Tous droits réservés</p>
+    
+    </footer>
+</body>
+</html>
